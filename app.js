4b469fbe80c6c9059b1d264765f3ac6ba08bb162
Ebubekir Aydın <ercandryl11@gmail.com>
2023-08-23 10:49:35 +0300

    Accepted2

--- app.js
+++ app.js
@@ -2,12 +2,61 @@ const express = require('express');
 
 const app = express();
 
+
+//TEMPLATE ENGİNE
+app.set("view engine", "ejs");
+
+
+//MIDDLEWARES
+app.use(express.static("public"));
+
+
+
+
+//ROUTES
 app.get('/', (req, res) => {
-  res.send('INDEX SAYFASİ');
+  res.status(200).render('index', {
+    page_name: "index"
+  });
 });
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: "about"
+  });
+})
+
 
 const port = 3000;
 
 app.listen(port, () => {
   console.log(`App started on ${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
