09220232d4fc7ab6cfe8e56316196545ff8757ef
Ebubekir Aydın <ercandryl11@gmail.com>
2023-08-23 18:01:16 +0300

    Accepted2

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
+
+
 
 const app = express();
+
+//CONNECT DB
+mongoose.connect('mongodb://localhost/smartedu-db', {
+}).then(() => {
+  console.log('DB connected successfuly')
+})
 
 
 //TEMPLATE ENGİNE
@@ -14,16 +25,10 @@ app.use(express.static("public"));
 
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.status(200).render('index', {
-    page_name: "index"
-  });
-});
-app.get('/about', (req, res) => {
-  res.status(200).render('about', {
-    page_name: "about"
-  });
-})
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
+
+
 
 
 const port = 3000;
